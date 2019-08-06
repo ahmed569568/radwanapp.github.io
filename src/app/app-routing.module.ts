ba121fd91a3472904d3ca8b10b7e614b0c96a236
shokry <shokrysuleiman032@gmail.com>
2019-08-06 23:05:18 +0200

    Create Contact Form And Contact Info In Contact Us Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,15 @@ import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { SearchComponent } from './search/search.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 
 const routes: Routes = [
       { path: '', component: HomeComponent },
       { path: 'home', component: HomeComponent},
       { path: 'search', component: SearchComponent },
-      { path: 'productDetails', component: ProductDetailsComponent}
+      { path: 'productDetails', component: ProductDetailsComponent},
+      { path: 'contact-us', component: ContactUsComponent }
 ];
 
 @NgModule({
