a3e25804e5cb9b3d013ccb362021dd5ecd180b17
shokry_suleiman <shokrysuleiman032@gmail.com>
2019-08-19 04:25:47 +0200

    Adding New Font And New Color

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -6,12 +6,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
+  menue: boolean;
+  menueList: Array<boolean> ;
+  constructor() { 
+     this.menue =false; 
+     this.menueList = [ false, false, false, false, false];
+  }
   ngOnInit() {
   }
   goTo() {
     console.log("shokryaaat")
+  }
+  showHeader(index) {
+    console.log("HEADer")
+    this.menue = true;
+    this.menueList.fill(false);
+    this.menueList[index]= true;
+    console.log(index);
+
+  }
+  hideHeader() {
+     this.menue = false;
+     this.menueList.fill(false);
   }
 }
