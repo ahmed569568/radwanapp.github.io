7bb804795351f64b603724b58921362d486dea1e
shokry <shokrysuleiman032@gmail.com>
2019-08-28 17:24:07 +0200

    Finish Menue List

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -10,25 +10,71 @@ import { Router } from '@angular/router';
 export class HeaderComponent implements OnInit {
   menue: boolean;
   menueList: Array<boolean> ;
+  headerList: boolean;
+  lastLink: any;
   constructor( private route: Router ) { 
      this.menue =false; 
      this.menueList = [ false, false, false, false, false];
+
   }
+
   ngOnInit( ) {
   }
+
   goTo() {
     this.route.navigate(['./search']);
   }
+
   showHeader(index) {
-    console.log("HEADer")
-    this.menue = true;
+    this.lastLink = index;
     this.menueList.fill(false);
     this.menueList[index]= true;
-    console.log(index);
-
+    
   }
+ 
   hideHeader() {
-     this.menue = false;
-     this.menueList.fill(false);
+    this.menueList.fill(false);
   }
+
+  showMenue() {
+    console.log("this.lastLink", this.lastLink);
+    let i = this.lastLink;
+    this.menue =true;
+    this.menueList[i] = true;
+    console.log("this.lastLink", this.menueList[i]);
+  }
+
+  hideMenue() {
+    console.log("now hide list");
+    if (!this.headerList) {
+      this.menue = false;
+      this.menueList.fill(false);
+    }
+      
+  }
+
+  overHeaderList() {
+    this.headerList = true;
+    this.menueList[this.lastLink] = true;
+    console.log("this.lastLink", this.menueList[this.lastLink]);
+    console.log("mouseOverHeader", this.headerList);
+  }
+
+  outHeaderList() {
+    this.headerList =  false;
+    console.log("mouseOutHeader", this.headerList);
+  }
+
+
+  hideMenueHL() {
+    
+    console.log("hideMenueHL()");
+    this.menue = false;
+    this.menueList.fill(false);
+  }
+  showData() {
+    this.menue= false;
+    this.menueList.fill(false);
+  }
+
 }
