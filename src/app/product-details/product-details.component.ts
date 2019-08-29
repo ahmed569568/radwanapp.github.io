8e2e88ba9460b667e281cf66e810f09a9983cbe4
shokry <shokrysuleiman032@gmail.com>
2019-08-29 19:26:28 +0200

    Finishing Image Gallary

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -10,121 +10,29 @@ declare var $:any;
   styleUrls: ['./product-details.component.scss']
 })
 export class ProductDetailsComponent implements OnInit {
-  galleryOptions: NgxGalleryOptions[];
-  galleryImages: NgxGalleryImage[];
-  images: Image[]; // init this value with your images
-  imageIndex = 1;
-  galleryId = 1;
-  autoPlay = true;
-  showArrows = true;
-  showDots = true;
+
   constructor() { }
 
   ngOnInit() {
-    this.galleryOptions = [
-      {
-          width: '600px',
-          height: '400px',
-          thumbnailsColumns: 4,
-          imageAnimation: NgxGalleryAnimation.Slide
-      },
-      // max-width 800
-      {
-          breakpoint: 800,
-          width: '100%',
-          height: '600px',
-          imagePercent: 80,
-          thumbnailsPercent: 20,
-          thumbnailsMargin: 20,
-          thumbnailMargin: 20
-      },
-      // max-width 400
-      {
-          breakpoint: 400,
-          preview: false
-      }
-  ];
 
-  this.galleryImages = [
-      {
-          small: 'https://assets.mspimages.in/wp-content/uploads/2019/03/Redmi-7-2-696x435.jpg',
-          medium: 'https://assets.mspimages.in/wp-content/uploads/2019/03/Redmi-7-2-696x435.jpg',
-          big: 'https://assets.mspimages.in/wp-content/uploads/2019/03/Redmi-7-2-696x435.jpg'
-      },
-      {
-          small: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3FhqqKkXN014RAPg2F5KtUb5h9nLX55-O6ZApg7WBK3HS5vtVJg',
-          medium: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3FhqqKkXN014RAPg2F5KtUb5h9nLX55-O6ZApg7WBK3HS5vtVJg',
-          big: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3FhqqKkXN014RAPg2F5KtUb5h9nLX55-O6ZApg7WBK3HS5vtVJg'
-      },
-      {
-          small: 'https://www.91-img.com/pictures/132944-v6-xiaomi-redmi-7-mobile-phone-large-1.jpg',
-          medium: 'https://www.91-img.com/pictures/132944-v6-xiaomi-redmi-7-mobile-phone-large-1.jpg',
-          big: 'https://www.91-img.com/pictures/132944-v6-xiaomi-redmi-7-mobile-phone-large-1.jpg'
-      }
-  ];
   }
 
-  buy(){
-    console.log("sfsdfsdfa");
-  }
-  next() {
-    console.log("next");
-  }
-  previous(){
-    console.log("previous");
-  }
+ 
 
   imagesRect: Image[] = [
-    new Image(
-      0,
-      {
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/milan-pegasus-gallery-statue.jpg',
-        description: 'Description 1'
-      },
-      { img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/thumbs/t-milan-pegasus-gallery-statue.jpg',
-      title: 'First image title',
-      alt: 'First image alt',
-      ariaLabel: 'First image aria-label' }
-    ),
-    new Image(1, { img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/pexels-photo-47223.jpeg' }, { img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/thumbs/t-pexels-photo-47223.jpg' }),
-    new Image(
-      2,
-      {
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/pexels-photo-52062.jpeg',
-        description: 'Description 3',
-        title: 'Third image title',
-        alt: 'Third image alt',
-        ariaLabel: 'Third image aria-label'
-      },
-      {
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/thumbs/t-pexels-photo-52062.jpg',
-        description: 'Description 3'
-      }
-    ),
-    new Image(
-      3,
-      {
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/pexels-photo-66943.jpeg',
-        description: 'Description 4',
-        title: 'Fourth image title (modal obj)',
-        alt: 'Fourth image alt (modal obj)',
-        ariaLabel: 'Fourth image aria-label (modal obj)'
-      },
-      { img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/thumbs/t-pexels-photo-66943.jpg',
-      title: 'Fourth image title (plain obj)',
-      alt: 'Fourth image alt (plain obj)',
-      ariaLabel: 'Fourth image aria-label (plain obj)' }
-    ),
-    new Image(4, { img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/pexels-photo-93750.jpeg' }, { img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/thumbs/t-pexels-photo-93750.jpg' }),
-    new Image(
-      5,
-      {
-        img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/pexels-photo-94420.jpeg',
-        description: 'Description 6'
-      },
-      { img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/thumbs/t-pexels-photo-94420.jpg' }
-    ),
-    new Image(6, { img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/pexels-photo-96947.jpeg' }, { img: 'https://raw.githubusercontent.com/Ks89/angular-modal-gallery/master/examples/systemjs/assets/images/gallery/thumbs/t-pexels-photo-96947.jpg' })
+    new Image(0, { img: 'assets/Tv.jpg', description: ''},
+      { img: 'assets/Tv.jpg', title: '',alt: '',ariaLabel: '' }),
+    new Image(1, { img: 'assets/Tv.jpg' }),
+    
+    new Image(2, { img: 'assets/Tv.jpg', description: '', title: '', alt: '', ariaLabel: '' },
+      { img: 'assets/Tv.jpg', description: ''}),
+    new Image(3,
+      { img: 'assets/Tv.jpg', description: '', title: '', alt: '', ariaLabel: ''},
+      { img: 'assets/Tv.jpg', title: '',alt: '',ariaLabel: '' }),
+    new Image(4, { img: 'assets/Tv.jpg' }),
+    new Image(5, { img: 'assets/Tv.jpg', description: ''},
+      { img: 'assets/Tv.jpg' }),
+    new Image(6, { img: 'assets/Tv.jpg' })
   ];
 
   accessibilityConfig: AccessibilityConfig = {
@@ -178,28 +86,4 @@ export class ProductDetailsComponent implements OnInit {
     this.imagesRect = [...this.imagesRect, newImage];
   }
 
-  onChangeAutoPlay() {
-    this.autoPlay = !this.autoPlay;
-  }
-
-  onChangeShowArrows() {
-    this.showArrows = !this.showArrows;
-  }
-
-  onChangeShowDots() {
-    this.showDots = !this.showDots;
-  }
-
-  // output evets
-  onShow(event: ImageEvent) {
-    console.log('show', event);
-  }
-
-  onFirstImage(event: ImageEvent) {
-    console.log('firstImage', event);
-  }
-
-  onLastImage(event: ImageEvent) {
-    console.log('lastImage', event);
-  }
 }
